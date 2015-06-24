804e08bc17c9caebdf2f392c7986649e1cbd6d0f
Jim Grant <jimgrant@gmail.com>
2015-06-24 16:07:18 -0700

    pep8 compliance

--- linked_list.py
+++ linked_list.py
@@ -1,87 +1,85 @@
 from __future__ import unicode_literals
 
-class Node(object):
-    #set default pointer to nonetype
-    def __init__(self, value, next = None):
-        self.value = value
-        self.next = next
 
-#Constructor
+class Node(object):
+    # set default pointer to nonetype
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next = next_node
+
+
+# Constructor
 class LinkedList(object):
     def __init__(self, values):
         self.headNode = None
         for value in values:
             self.insert(value)
 
-    #String representation
+    # String representation
     def __repr__(self):
         printstr = "("
-        tempNode = self.headNode
-        while tempNode != None:
+        temp_node = self.headNode
+        while temp_node is not None:
             if printstr != "(":
-                printstr = "{},{}".format(printstr, tempNode.value)
+                printstr = "{},{}".format(printstr, temp_node.value)
             else:
-                printstr += str(tempNode.value)
-            tempNode = tempNode.next
+                printstr += str(temp_node.value)
+            temp_node = temp_node.next
         printstr += ")"
         return printstr
 
-    #Returns size of linked list
+    # Returns size of linked list
     def size(self):
         size = 0
-        #sets temporary pointer on current head node
-        tempNode = self.headNode
-        while tempNode != None:
+        # sets temporary pointer on current head node
+        temp_node = self.headNode
+        while temp_node is not None:
             size += 1
-            #iterate through head node
-            tempNode = tempNode.next
+            # iterate through head node
+            temp_node = temp_node.next
         return size
 
-    #returns a string representation of all values
+    # returns a string representation of all values
     def display(self):
-         return str(self)
+        return str(self)
 
-    #searches to see if value exists, returns node of passed-in value
+    # searches to see if value exists, returns node of passed-in value
     def search(self, value):
         iterations = self.size()
-        tempNode = self.headNode
+        temp_node = self.headNode
         for x in range(0,iterations):
-            if tempNode.value == value:
-                return tempNode
-            tempNode = tempNode.next
+            if temp_node.value == value:
+                return temp_node
+            temp_node = temp_node.next
         return None
 
-    #Insert value at the head of the list
+    # insert value at the head of the list
     def insert(self, value):
-        tempNode = Node(value,self.headNode)
-        self.headNode = tempNode
+        temp_node = Node(value,self.headNode)
+        self.headNode = temp_node
 
-    #Pops the first value
+    # pop the first value
     def pop(self):
-        tempNode = self.headNode
+        temp_node = self.headNode
         self.headNode = self.headNode.next
-        tempNode.next = None
-        return tempNode.value
+        temp_node.next = None
+        return temp_node.value
 
-    #remove node based on  given node
+    # remove node based on  given node
     def remove(self, node):
-        tempNode = self.headNode
-        tempNode2 = self.headNode.next
-        if tempNode == node:
+        temp_node = self.headNode
+        temp_node2 = self.headNode.next
+        if temp_node == node:
             self.headNode = self.headNode.next
-            tempNode.next = None
-
+            temp_node.next = None
         else:
-            while tempNode2 != None:
-                if tempNode2 == node:
-                    tempNode.next = tempNode2.next
-                    tempNode2.next = None
+            while temp_node2 is not None:
+                if temp_node2 == node:
+                    temp_node.next = temp_node2.next
+                    temp_node2.next = None
                     break
                 else:
-                    tempNode = tempNode.next
-                    tempNode2 = tempNode2.next
+                    temp_node = temp_node.next
+                    temp_node2 = temp_node2.next
             else:
                 raise ValueError('node not found')
-
-
-
--- test_linked_list.py
+++ test_linked_list.py
@@ -2,29 +2,35 @@ from __future__ import unicode_literals
 import pytest
 from linked_list import LinkedList
 
-list1 = LinkedList([1,2,3,4,5,6,7,8])
+list1 = LinkedList([1, 2, 3, 4, 5, 6, 7, 8])
+
 
 def test_const():
-    assert list1.headNode != None
+    assert list1.headNode is not None
     assert list1.headNode.value == 8
     assert list1.headNode.next.value == 7
+
 
 def test_size():
     assert list1.size() == 8
 
+
 def test_display():
     assert list1.display() == "(8,7,6,5,4,3,2,1)"
 
+
 def test_search():
-    assert list1.search(3) != None
-    assert list1.search(2) != None
-    assert list1.search(50) == None
+    assert list1.search(3) is not None
+    assert list1.search(2) is not None
+    assert list1.search(50) is None
+
 
 def test_insert():
     assert list1.size() == 8
     list1.insert(5)
     assert list1.display() == "(5,8,7,6,5,4,3,2,1)"
     assert list1.size() == 9
+
 
 def test_pop():
     assert list1.pop() == 5
@@ -34,27 +40,26 @@ def test_pop():
     assert list1.size() == 7
     assert list1.display() == "(7,6,5,4,3,2,1)"
 
+
 def test_remove():
-    tempNode = list1.search(1)
-    list1.remove(tempNode)
-    assert list1.search(1) == None
-    assert list1.size() ==  6
+    tempnode = list1.search(1)
+    list1.remove(tempnode)
+    assert list1.search(1) is None
+    assert list1.size() == 6
     assert list1.display() == "(7,6,5,4,3,2)"
-    tempNode = list1.search(7)
-    list1.remove(tempNode)
-    assert list1.search(7) == None
-    assert list1.size() ==  5
+    tempnode = list1.search(7)
+    list1.remove(tempnode)
+    assert list1.search(7) is None
+    assert list1.size() == 5
     assert list1.display() == "(6,5,4,3,2)"
-    tempNode = list1.search(4)
-    list1.remove(tempNode)
-    assert list1.search(4) == None
-    assert list1.size() ==  4
+    tempnode = list1.search(4)
+    list1.remove(tempnode)
+    assert list1.search(4) is None
+    assert list1.size() == 4
     assert list1.display() == "(6,5,3,2)"
-    tempNode = list1.search(7)
+    tempnode = list1.search(7)
     with pytest.raises(ValueError):
-        list1.remove(tempNode)
-    assert list1.search(7) == None
-    assert list1.size() ==  4
+        list1.remove(tempnode)
+    assert list1.search(7) is None
+    assert list1.size() == 4
     assert list1.display() == "(6,5,3,2)"
-
-
