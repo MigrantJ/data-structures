f908bc880bfae76543d2a05cb5cab5561ad0c8c3
Jim Grant <jimgrant@gmail.com>
2015-06-27 07:48:24 -0700

    refactor for efficiency. add docstring and comments

--- parenthetical.py
+++ parenthetical.py
@@ -3,19 +3,27 @@ from stack.stack import Stack
 
 
 def parens(string):
-    stack = Stack(str(string)[::-1])
-    balance = 0
-    while True:
-        try:
-            c = stack.pop()
-        except LookupError:
-            break
+    """Return an integer representing whether all open parentheses are closed
+    in order in the provided input.
 
+    Args:
+        string (unicode): The string to search through for parens.
+
+    Returns:
+        int: Value represents whether parens are matched:
+            1: There are open parens that are not closed.
+            0: There are an equal number of matched open and close parens.
+            -1: There is a closed paren before a matching open paren.
+    """
+
+    stack = Stack([-1, 0])
+    # ensure input is proper type before iterating
+    for c in unicode(string):
         if c == '(':
-            balance += 1
+            stack.push(1)
         elif c == ')':
-            balance -= 1
+            if stack.pop() == 0:
+                # this is a 'broken' string
+                break
 
-        if balance < 0:
-            break
-    return min(balance, 1)
+    return stack.pop()
