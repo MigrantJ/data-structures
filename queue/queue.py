65006de2eb33dbd7fcef384370a3ab5dc3a91b74
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-06-29 13:48:24 -0700

    queue class

--- queue/queue.py
+++ queue/queue.py
@@ -9,14 +9,31 @@ class Node(object):
 
 
 # Constructor
-class LinkedList(object):
+class Queue(object):
     def __init__(self, values=None):
         self.headNode = None
-        if values is not None:
-            for value in values:
-                self.insert(value)
+        self.tailNode = None
 
-    # String representation
+    def enqueue(self, value):
+        tempNode = Node(value)
+        if self.size() == 0:
+            self.headNode = tempNode
+            self.tailNode = tempNode
+        else:
+            self.tailNode.next = tempNode
+            self.tailNode = tempNode
+
+    def dequeue(self):
+        tempNode = self.headNode
+        if self.size() == 0:
+            raise LookupError
+        while (tempNode.next != None):
+            tempNode = tempNode.next
+        returnVal = self.tailNode.value
+        tempNode.next = None
+        self.tailNode = tempNode
+        return returnVal
+
     def __repr__(self):
         printstr = "("
         temp_node = self.headNode
@@ -33,56 +50,11 @@ class LinkedList(object):
     def size(self):
         size = 0
         # sets temporary pointer on current head node
-        temp_node = self.headNode
-        while temp_node is not None:
+        tempNode = self.headNode
+        while tempNode is not None:
             size += 1
             # iterate through head node
-            temp_node = temp_node.next
+            tempNode = tempNode.next
         return size
 
-    # returns a string representation of all values
-    def display(self):
-        return repr(self)
 
-    # searches to see if value exists, returns node of passed-in value
-    def search(self, value):
-        iterations = self.size()
-        temp_node = self.headNode
-        for x in range(0,iterations):
-            if temp_node.value == value:
-                return temp_node
-            temp_node = temp_node.next
-        return None
-
-    # insert value at the head of the list
-    def insert(self, value):
-        temp_node = Node(value,self.headNode)
-        self.headNode = temp_node
-
-    # pop the first value
-    def pop(self):
-        if self.size() == 0:
-            raise LookupError
-        temp_node = self.headNode
-        self.headNode = self.headNode.next
-        temp_node.next = None
-        return temp_node.value
-
-    # remove node based on  given node
-    def remove(self, node):
-        temp_node = self.headNode
-        temp_node2 = self.headNode.next
-        if temp_node == node:
-            self.headNode = self.headNode.next
-            temp_node.next = None
-        else:
-            while temp_node2 is not None:
-                if temp_node2 == node:
-                    temp_node.next = temp_node2.next
-                    temp_node2.next = None
-                    break
-                else:
-                    temp_node = temp_node.next
-                    temp_node2 = temp_node2.next
-            else:
-                raise ValueError('node not found')
