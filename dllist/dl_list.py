05ab83a93fc78eff7e152bcc81139bdc937410ae
Jim Grant <jimgrant@gmail.com>
2015-06-30 13:09:26 -0700

    implement insert. add default prev and next to dlnode constructor

--- dllist/dl_list.py
+++ dllist/dl_list.py
@@ -3,18 +3,24 @@ from __future__ import unicode_literals
 
 
 class DLNode(object):
-    def __init__(self, value):
+    def __init__(self, value, next=None, prev=None):
         self.value = value
-        self.next = None
-        self.prev = None
+        self.next = next
+        self.prev = prev
 
 
 class DLList(object):
     def __init__(self):
         self.head_node = None
+        self.tail_node = None
 
     def insert(self, val):
-        pass
+        node = DLNode(val, self.head_node)
+        if self.head_node:
+            temp = self.head_node
+            temp.prev = node
+
+        self.head_node = node
 
     def append(self, val):
         pass
