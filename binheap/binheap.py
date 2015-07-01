b5326c3f2aafcc61e18b2295c0eb715d03eca826
Jim Grant <jimgrant@gmail.com>
2015-07-01 13:39:04 -0700

    class skeleton

---
+++ binheap/binheap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class BinHeap(object):
+    """Implementation of a min binary heap."""
+    def __init__(self):
+        self.tree = []
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
