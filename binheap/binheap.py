f8a6694773c4a8113ede81646163459181e841dc
Jim Grant <jimgrant@gmail.com>
2015-07-01 13:47:32 -0700

    initial implementation of push. internal method sort_up

--- binheap/binheap.py
+++ binheap/binheap.py
@@ -7,8 +7,19 @@ class BinHeap(object):
     def __init__(self):
         self.tree = []
 
-    def push(self):
-        pass
+    def __repr__(self):
+        print self.tree
+
+    def sort_up(self, index):
+        p_index = (index - 1) // 2
+
+        if self.tree[index] < self.tree[p_index]:
+            self.tree[p_index], self.tree[index] = self.tree[index], self.tree[p_index]
+            self.sort_up(p_index)
+
+    def push(self, val):
+        self.tree.append(val)
+        self.sort_up(len(self.tree) - 1)
 
     def pop(self):
         pass
