bcec3fdb91041c11914e7e95f037c9881ab2616f
Jim Grant <jimgrant@gmail.com>
2015-07-01 14:15:07 -0700

    add swap utility function. first pass implementation of pop. start on sort_down

--- binheap/binheap.py
+++ binheap/binheap.py
@@ -10,6 +10,18 @@ class BinHeap(object):
     def __repr__(self):
         return repr(self.tree)
 
+    def swap(self, i1, i2):
+        self.tree[i1], self.tree[i2] = self.tree[i2], self.tree[i1]
+
+    def sort_down(self, p_index):
+        lc_index = p_index * 2 + 1
+        rc_index = lc_index + 1
+
+        if lc_index < rc_index:
+            print "hi"
+
+        return 0
+
     def sort_up(self, c_index):
         p_index = (max(0, c_index - 1)) // 2
         # print("Parent Index: " + str(p_index))
@@ -17,7 +29,7 @@ class BinHeap(object):
 
         if self.tree[c_index] < self.tree[p_index]:
             # print("------")
-            self.tree[p_index], self.tree[c_index] = self.tree[c_index], self.tree[p_index]
+            self.swap(p_index, c_index)
             self.sort_up(p_index)
 
     def push(self, val):
@@ -25,4 +37,8 @@ class BinHeap(object):
         self.sort_up(len(self.tree) - 1)
 
     def pop(self):
-        pass
+        last = len(self.tree) - 1
+        self.swap(0, last)
+        val = self.tree.pop()
+        self.sort_down(0)
+        return val
