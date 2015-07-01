2ddd7add0e32b3176d43e62984b8caddf1c7f85a
Jim Grant <jimgrant@gmail.com>
2015-07-01 15:05:52 -0700

    sort_down works for odd-numbered length heaps

--- binheap/binheap.py
+++ binheap/binheap.py
@@ -17,28 +17,27 @@ class BinHeap(object):
         lc_index = p_index * 2 + 1
         rc_index = lc_index + 1
 
-        print("Parent Index: " + str(p_index))
-        print("Left Child Index: " + str(lc_index))
-        print("Right Child Index: " + str(rc_index))
-        print("--------")
-
         try:
-            if self.tree[lc_index] <= self.tree[rc_index]:
+            lc_val = self.tree[lc_index]
+            try:
+                rc_val = self.tree[rc_index]
+            except IndexError:
                 self.swap(p_index, lc_index)
-                self.sort_down(lc_index)
-            elif self.tree[lc_index] > self.tree[rc_index]:
-                self.swap(p_index, rc_index)
-                self.sort_down(rc_index)
+                return
+            else:
+                if self.tree[lc_index] <= self.tree[rc_index]:
+                    self.swap(p_index, lc_index)
+                    self.sort_down(lc_index)
+                elif self.tree[lc_index] > self.tree[rc_index]:
+                    self.swap(p_index, rc_index)
+                    self.sort_down(rc_index)
         except IndexError:
             return
 
     def sort_up(self, c_index):
         p_index = (max(0, c_index - 1)) // 2
-        # print("Parent Index: " + str(p_index))
-        # print("Child Index: " + str(c_index))
 
         if self.tree[c_index] < self.tree[p_index]:
-            # print("------")
             self.swap(p_index, c_index)
             self.sort_up(p_index)
 
