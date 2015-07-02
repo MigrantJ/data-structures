c2b26c982b94a07c6ad69441da642b56609f2d5a
Jim Grant <jimgrant@gmail.com>
2015-07-02 15:23:19 -0700

    add order tracking to nodes. stop peek modifying the binheap

--- binheap/priorityq.py
+++ binheap/priorityq.py
@@ -4,32 +4,38 @@ from binheap import BinHeap
 
 @total_ordering
 class Node(object):
-    def __init__(self, priority, val):
+    def __init__(self, priority, val, order):
         self.priority = priority
         self.val = val
+        self.order = order
 
     def __repr__(self):
         return 'Pri: ' + repr(self.priority) + ' Val: ' + repr(self.val)
 
     def __eq__(self, other):
-        return self.priority == other.priority
+        if self.priority == other.priority:
+            return self.order == other.order
 
     def __lt__(self, other):
-        return self.priority < other.priority
+        if self.priority == other.priority:
+            return self.order < other.order
+        else:
+            return self.priority < other.priority
 
 
 class Priorityq(object):
     def __init__(self):
         self.binheap = BinHeap()
+        self._count = 0
 
     def insert(self, priority, val):
-        new_node = Node(priority, val)
+        new_node = Node(priority, val, self._count)
         self.binheap.push(new_node)
+        self._count += 1
 
     def pop(self):
         return_node = self.binheap.pop()
         return return_node.val
 
     def peek(self):
-        return_node = self.binheap.pop()
-        return return_node.val
+        return self.binheap[0]
