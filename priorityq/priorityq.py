b4f0a0d8c8a6409a17734518c3683d95f29de0c3
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-07-02 14:02:31 -0700

    all functions for pq

--- priorityq/priorityq.py
+++ priorityq/priorityq.py
@@ -1,1 +1,30 @@
+from functools import total_ordering
 from binheap import BinHeap
+
+
+@total_ordering
+class Node(object):
+    def __init__(self, priority, val):
+        self.priority = priority
+        self.val = val
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
+class Priorityq(object):
+    def __init__(self):
+        self.binheap = BinHeap()
+
+    def insert(self, priority, val):
+        new_node = Node(priority, val)
+        self.binheap.insert(new_node)
+
+    def pop(self):
+        return self.binheap.pop()
+
+    def peak(self):
+        return self.binheap[0]
