416357bea4f6cf96627c57cc608298e06f84454c
Jim Grant <jimgrant@gmail.com>
2015-07-02 15:24:43 -0700

    full_queue fixture really isn't needed

--- binheap/test_priorityq.py
+++ binheap/test_priorityq.py
@@ -8,20 +8,6 @@ def empty_q():
     return Priorityq()
 
 
-@pytest.fixture()
-def full_q():
-    q = Priorityq()
-    # values are the order we expect to receive them in when we pop
-    q.insert(3, 3)
-    q.insert(0, 0)
-    q.insert(2, 2)
-    q.insert(10, 6)
-    q.insert(3, 4)
-    q.insert(5, 5)
-    q.insert(0, 1)
-    return q
-
-
 def test_pop_empty(empty_q):
     with pytest.raises(LookupError):
         empty_q.pop()
@@ -44,10 +30,15 @@ def test_equal_priority(empty_q):
     empty_q.insert(0, 0)
     empty_q.insert(0, 1)
     empty_q.insert(0, 2)
-    import pdb; pdb.set_trace()
+    empty_q.insert(0, 3)
+    empty_q.insert(0, 4)
+    empty_q.insert(0, 5)
     assert empty_q.pop() == 0
     assert empty_q.pop() == 1
     assert empty_q.pop() == 2
+    assert empty_q.pop() == 3
+    assert empty_q.pop() == 4
+    assert empty_q.pop() == 5
 
 
 def test_odd_values(empty_q):
@@ -57,3 +48,17 @@ def test_odd_values(empty_q):
     assert empty_q.pop() is None
     assert empty_q.pop() == 'One'
     assert empty_q.pop() == [2, 3]
+
+
+def test_full_pop(empty_q):
+    # values are the order we expect to receive them in when we pop
+    empty_q.insert(3, 3)
+    empty_q.insert(0, 0)
+    empty_q.insert(2, 2)
+    empty_q.insert(10, 6)
+    empty_q.insert(3, 4)
+    empty_q.insert(5, 5)
+    empty_q.insert(0, 1)
+
+    for i in range(0, 7):
+        assert empty_q.pop() == i
