56802619f5ff61a30c3991484ef0e36293edbc64
Jim Grant <jimgrant@gmail.com>
2015-07-06 13:08:20 -0700

    inits for node and graph, base definitions of methods

--- graph/graph.py
+++ graph/graph.py
@@ -3,8 +3,37 @@ from __future__ import unicode_literals
 
 
 class Node(object):
-    pass
+    def __init__(self, value):
+        self.value = value
 
 
 class Graph(object):
-    pass
+    def __init__(self):
+        self._data = {}
+
+    def nodes(self):
+        pass
+
+    def edges(self):
+        pass
+
+    def add_node(self, value):
+        pass
+
+    def add_edge(self, n1, n2):
+        pass
+
+    def del_node(self, n):
+        pass
+
+    def del_edge(self, n1, n2):
+        pass
+
+    def has_node(self, n):
+        pass
+
+    def neighbors(self, n):
+        pass
+
+    def adjacent(self, n1, n2):
+        pass
