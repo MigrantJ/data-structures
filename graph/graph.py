08402f3250fb285d0d2968c1d653603badc2ee9d
Jim Grant <jimgrant@gmail.com>
2015-07-06 13:16:56 -0700

    add repr to node and graph. implement add_node and del_node

--- graph/graph.py
+++ graph/graph.py
@@ -6,10 +6,19 @@ class Node(object):
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self):
+        return repr(self.value)
+
 
 class Graph(object):
     def __init__(self):
         self._data = {}
+
+    def __repr__(self):
+        rep = []
+        for k, v in self._data.iteritems():
+            rep.append('{}: {}'.format(k, v))
+        return '\n'.join(rep)
 
     def nodes(self):
         pass
@@ -18,13 +27,13 @@ class Graph(object):
         pass
 
     def add_node(self, value):
-        pass
+        self._data[Node(value)] = []
 
     def add_edge(self, n1, n2):
         pass
 
     def del_node(self, n):
-        pass
+        del self._data[n]
 
     def del_edge(self, n1, n2):
         pass
