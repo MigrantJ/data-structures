947c217462896f203c603baac19f8edaaa4124e4
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-07-20 13:51:28 -0700

    wgraph initial

---
+++ graph/wgraph.py
@@ -0,0 +1,99 @@
+from __future__ import unicode_literals
+from queue import Queue
+from graph import Node
+
+
+class Graph(object):
+    def __init__(self):
+        self._data = {}
+
+    def __repr__(self):
+        rep = []
+        for k, v in self._data.iteritems():
+            rep.append('{}: {}'.format(k, v))
+        return '\n'.join(rep)
+
+    def nodes(self):
+        """Return a list of all nodes in the graph."""
+        return [k for k in self._data.iterkeys()]
+
+    def edges(self):
+        """Return a list of all edges in the graph. Edges are represented by a
+        tuple, with the first item being the origin node and the second item
+        being the target.
+        """
+        edgelist = []
+        for node, others in self._data.iteritems():
+            for o in others:
+                edgelist.append((node, o))
+        return edgelist
+
+    def add_node(self, n):
+        """Add a new node 'n' to the graph. n must be an instance of Node."""
+        self._data.setdefault(n, {})
+
+    def add_edge(self, n1, n2, weight):
+        """Add a new edge to the graph connecting n1 to n2. n1 and n2 must be
+        instances of Node. If n1 or n2 are not in the graph, they are added.
+        """
+        if not self.has_node(n1):
+            self.add_node(n1)
+
+        if not self.has_node(n2):
+            self.add_node(n2)
+
+        self._data[n1].setdefault(n2, weight)
+
+    def del_node(self, n):
+        """Delete node n from the graph. If no such node exists, raise
+        KeyError.
+        """
+        for node, e in self._data.iteritems():
+            e.pop(n)
+        del self._data[n]
+
+    def del_edge(self, n1, n2):
+        """Delete the edge connecting n1 to n2. If n1 does not exist in the
+        graph, raise KeyError. If n1 is not connected to n2, raise KeyError.
+        """
+        self._data[n1].pop(n2)
+
+    def has_node(self, n):
+        """Return True if node n is in the graph, False if not."""
+        return n in self._data
+
+    def neighbors(self, n):
+        """Return a list of all nodes connected to n by edges."""
+        return self._data[n]
+
+    def adjacent(self, n1, n2):
+        """Return True if an edge connects n1 to n2, return False if not.
+        If n1 or n2 is not in the graph, raise KeyError.
+        """
+        return n2 in self._data[n1]
+
+    def depth_first_traversal(self, start, return_set=None):
+        if return_set is None:
+            return_set = []
+        temp_node = start
+        if temp_node not in return_set:
+            return_set.append(temp_node)
+        for temp_node in self.neighbors(start):
+            if temp_node not in return_set:
+                self.depth_first_traversal(temp_node, return_set)
+        return return_set
+
+    def breadth_first_traversal(self, start, return_set=None):
+        if return_set is None:
+            return_set = []
+        temp_queue = Queue()
+        if start not in return_set:
+            return_set.append(start)
+        for temp_node in self.neighbors(start):
+            if temp_node not in return_set:
+                return_set.append(temp_node)
+                temp_queue.enqueue(temp_node)
+        while temp_queue.size() != 0:
+            temp_node = temp_queue.dequeue()
+            self.breadth_first_traversal(temp_node, return_set)
+        return return_set
