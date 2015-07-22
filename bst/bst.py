a51b4b517fda54b69f712de5a54bc32be76d9c07
Jim Grant <jimgrant@gmail.com>
2015-07-22 13:26:33 -0700

    initial implementation of insert

--- bst/bst.py
+++ bst/bst.py
@@ -1,17 +1,51 @@
 class Node():
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
 
 
 class Tree():
     def __init__(self):
-        pass
-    
+        self._head = None
+        self._size = 0
+        self._depth = 0
+        self._balance = 0
+
     def insert(self, value):
         # insert value into tree. if already present, ignored
-        pass
+        n = Node(value)
+        if self._head is None:
+            self._head = n
+            return
+
+        parent = None
+        direction = 'left'
+        current = self._head
+        depth = 0
+        while current is not None:
+            parent = current
+            if n.value < current.value:
+                current = current.left
+                direction = 'left'
+            elif n.value > current.value:
+                current = current.right
+                direction = 'right'
+            else:
+                return
+            depth += 1
+
+        if depth > self._depth:
+            self._depth = depth
+
+        if direction == 'left':
+            parent.left = n
+            self._balance += 1
+        else:
+            parent.right = n
+            self._balance -= 1
+
+        self._size += 1
 
     def contains(self, value):
         # return true if value in tree, false if not
@@ -19,14 +53,14 @@ class Tree():
 
     def size(self):
         # return total number of values stored in tree
-        pass
+        return self._size
 
     def depth(self):
         # return int of total number of tree levels
-        pass
+        return self._depth
 
     def balance(self):
         # return positive int if more values on left
         # negative if more values on right
         # 0 if balanced
-        pass
+        return self._balance
