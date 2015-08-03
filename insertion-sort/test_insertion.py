5b87996fb6b5646496cc85b9d870c93b120a7a59
Megan Slater <megan.e.slater@gmail.com>
2015-08-03 13:25:03 -0700

    added tests for insertion sort

---
+++ insertion-sort/test_insertion.py
@@ -0,0 +1,21 @@
+from insertion import insertion_sort
+
+
+def test_random_list():
+    lst = insertion_sort([4, 3, 8, 5, 0, 2, 56, 9, 68, -1])
+    assert lst == [-1, 0, 2, 3, 4, 5, 8, 9, 56, 68]
+
+
+def test_empty_list():
+    lst = insertion_sort([])
+    assert lst == []
+
+
+def test_alphabet():
+    lst = insertion_sort(['a', 'b', 'f', 'e', 'y', 'w', 'i', 'c', 'p'])
+    assert lst == ['a', 'b', 'c', 'e', 'f', 'i', 'p', 'w', 'y']
+
+
+def test_empty_string():
+    lst = insertion_sort([2, 5, 'a', 7, 'd', 0, '', 'o'])
+    assert lst == [0, 2, 5, 7, '', 'a', 'd', 'o']
