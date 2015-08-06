f995fb7a3a32af8d0df1f2d4d4f1720c2b7b56be
Megan Slater <megan.e.slater@gmail.com>
2015-08-06 12:58:50 -0700

    first pass at radix tests

---
+++ radix-sort/test_radix_sort.py
@@ -0,0 +1,48 @@
+import radix_sort as r
+import pytest
+
+
+@pytest.fixture()
+def randlist():
+    return [4, 3, 8, 5, 0, 2, 56, 9, 68, -1]
+
+
+@pytest.fixture()
+def randlist_sorted():
+    return [-1, 0, 2, 3, 4, 5, 8, 9, 56, 68]
+
+
+def test_random_list(randlist, randlist_sorted):
+    lst = randlist[:]
+    new_lst = lst.r.radix_sort(lst)
+    assert new_lst == randlist_sorted
+
+
+def test_empty_list():
+    lst = []
+    new_lst = r.radix_sort(lst)
+    assert new_lst == []
+
+
+def test_list_of_one():
+    lst = [1]
+    new_lst = r.radix_sort(lst)
+    assert new_lst == [1]
+
+
+def test_duplicates():
+    lst = [1, 2, 1, 2, 1, 2, 1, 2]
+    new_lst = r.radix_sort(lst)
+    assert new_lst == [1, 1, 1, 1, 2, 2, 2, 2]
+
+
+def test_all_duplicates():
+    lst = [1, 1, 1, 1]
+    new_lst = r.radix_sort(lst)
+    assert new_lst == [1, 1, 1, 1]
+
+
+def test_alphabet():
+    lst = ['a', 'b', 'f', 'e', 'y', 'w', 'i', 'c', 'p']
+    new_lst = r.radix_sort(lst)
+    assert new_lst == ['a', 'b', 'c', 'e', 'f', 'i', 'p', 'w', 'y']
